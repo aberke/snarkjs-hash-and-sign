062e87659e3aa4a642bbb9cdbf0bdcfba1e3642d
Alex Berke <aberke@dhcp-10-31-19-47.dyn.mit.edu>
2023-04-25 18:54:06 -0400

    work in progress
    - still need to export the zkey and verification files for end-to-end prototype
    - will require more powers of tau
    - for now can play in zkrepl using hash_and_sign_json_mimc_circuit.circom

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # React + snarkjs
-This is a minimal project to get react (typescript) and snarkjs up and running. The circuit is the example from the readme of https://github.com/iden3/snarkjs.
+
+Hash and sign 
 
 To install the necessary packages etc, run `yarn install`. 
 
@@ -16,4 +17,31 @@ yarn start
 ```
 
 The circuit and related files are located in the folder `src/zkproof`. 
+
+## Notes from Alex changes
+
+To fix ssl issue:
+`export NODE_OPTIONS=--openssl-legacy-provider`
+
+`yarn start`
+
+http://localhost:3000/
+
+
+[Helpful diagram of snark](https://file.notion.so/f/s/d08b94e9-ceac-4497-97b4-be481ea5f014/Untitled.png?id=4edc02c2-caf1-47b5-9084-664d56e87382&table=block&spaceId=49789257-8634-4c86-a9e2-dcecb65edf1c&expirationTimestamp=1682116775286&signature=Lbun12RN0PDWBRfxUByA5IReWqFuSMKccOuD-JU0vTo)
+
+Instructions to compile a circuit are here:
+https://github.com/iden3/snarkjs#10-compile-the-circuit
+
+### Work in progress notes
+
+witness successfully computes for hash_and_sign_json_mimc_circuit
+
+% snarkjs wtns check hash_and_sign_json_mimc_circuit.r1cs hash_and_sign_json_witness.wtns
+
+
+### TODO
+
+Move my powers of tau over here
+
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
 import "./App.css";
 
-import { Field, Input, Text, Button, Link } from "rimble-ui";
+// I got rid of rimble-ui because of dependency issues
+//import { Field, Input, Text, Button, Link } from "rimble-ui";
 
 const snarkjs = require("snarkjs");
+const fs = require("fs");
+const { buildBabyjub, buildMimc7, buildEddsa } = require("circomlibjs");
 
-const getSolidityProofArray = (proof: any) => {
-	let proofList = [
-		proof["pi_a"][0],
-		proof["pi_a"][1],
-		proof["pi_b"][0][1],
-		proof["pi_b"][0][0],
-		proof["pi_b"][1][1],
-		proof["pi_b"][1][0],
-		proof["pi_c"][0],
-		proof["pi_c"][1],
-	];
-	return proofList;
-};
+const MAX_JSON_SIZE = 1000; // TODO: increase the size after testing (slower)
+
+// utilities
+export type Ascii = number;
+export function toAsciiArray(str: string): Ascii[] {
+    return Array.from(str).map((_, i) => str.charCodeAt(i));
+}
+
+const isValidJson = (jsonInput) => {
+	try {
+		JSON.parse(jsonInput);
+		return true;
+	} catch (ex) {
+		return false;
+	}
+}
+
 
 const makeProof = async (_proofInput: any, _wasm: string, _zkey: string) => {
 	const { proof, publicSignals } = await snarkjs.groth16.fullProve(_proofInput, _wasm, _zkey);
@@ -28,7 +35,6 @@ const verifyProof = async (_verificationkey: string, signals: any, proof: any) =
 	const vkey = await fetch(_verificationkey).then(function (res) {
 		return res.json();
 	});
-
 	const res = await snarkjs.groth16.verify(vkey, signals, proof);
 	return res;
 };
@@ -36,6 +42,7 @@ const verifyProof = async (_verificationkey: string, signals: any, proof: any) =
 function App() {
 	const [a, setA] = useState("3");
 	const [b, setB] = useState("11");
+	const [jsonInput, setJsonInput] = useState(`{"name": "aberke"}`);
 
 	const [proof, setProof] = useState("");
 	const [signals, setSignals] = useState("");
@@ -61,33 +68,120 @@ function App() {
 			});
 		});
 	};
-
 	const changeA = (e) => {
 		setA(e.target.value);
 	};
-
 	const changeB = (e) => {
 		setB(e.target.value);
 	};
 
+
+
+	const handleJsonInput = async () => {
+		console.log('handleJsonInput')
+		// Transform JSON input to a string of ascii bytes
+		// Check if this is valid JSON (though we do not need it as an object for now)
+		if (!isValidJson(jsonInput)) {
+			console.log('invalid json input:', jsonInput); // TODO: error handling
+			return false;
+		}
+		// Convert to object and back to string in order to have consistent representation
+		// e.g. consistent key sorting, removed whitespace
+		const parsedJson = JSON.stringify(JSON.parse(jsonInput));
+		// Array of integers representing ascii characters.
+		// Note: Doesn't matter if this is array of integers or strings of integers
+		// i.e. {"test":[1,0]} same as {"test":["1", "0"]}
+		const jsonAsciiArray = toAsciiArray(parsedJson);
+		console.log('jsonAsciiArray')
+		console.log(jsonAsciiArray);
+		// Pad the ascii array to the predetermined size.
+		// This is necessary because a predetermined size must be used in compiled circuit.
+		// First check it does not exceed this size.
+		if (jsonAsciiArray.length > MAX_JSON_SIZE) {
+			console.error('Cannot handle json: too large. Max size:', MAX_JSON_SIZE, 'Json array size:', jsonAsciiArray.length);
+			return false;
+		}
+		const jsonSize = jsonAsciiArray.length; // Size before padding
+		const paddingLength = MAX_JSON_SIZE - jsonSize;
+		const paddedInput = jsonAsciiArray.concat(Array(paddingLength).fill(0));
+		console.log('paddedInput')
+		console.log(paddedInput)
+
+		const mimc7 = await buildMimc7();
+		const babyJub = await buildBabyjub();
+		const F = babyJub.F;
+
+		const k = 1;
+		const hash = mimc7.multiHash(paddedInput, k);
+
+		// handle signature
+		const eddsa = await buildEddsa();
+		console.log('generating Eddsa (sk, pk) key pair')
+		const sk = Buffer.from("1".toString().padStart(64, "0"), "hex");
+		const pk = eddsa.prv2pub(sk);
+		console.log('public key:', pk);
+		// do the signing
+		const signature = eddsa.signMiMC(sk, hash);
+
+		const inputs = {
+			json: paddedInput,
+			expected_hash: BigInt(F.toObject(hash)).toString(),
+			pubkey: [
+				BigInt(F.toObject(pk[0])).toString(),
+				BigInt(F.toObject(pk[1])).toString(),
+			],
+			signature_R8x: BigInt(F.toObject(signature.R8[0])).toString(),
+			signature_R8y: BigInt(F.toObject(signature.R8[1])).toString(),
+			signature_S: BigInt(signature.S).toString(),
+		};
+		console.log('inputs')
+		console.log(JSON.stringify(inputs))
+	}
+
+	const handleSignature = () => {
+		// TODO: move things here
+	}
+
+	const changeJsonInput = (e) => {
+		setJsonInput(e.target.value);
+	}
+
 	return (
 		<div>
 			<header className="App-header">
-				<Text>
-					The underlying circuit is from the <a href="https://github.com/iden3/snarkjs">snarkjs readme</a>
-				</Text>
+				<p>
+					Hash and sign JSON with MIMC - testing
+				</p>
+
 				<pre>Witness Inputs</pre>
+
+				<textarea 
+					label="json input" rows="4"
+					type="text" required={true} value={jsonInput} 
+					onChange={changeJsonInput} placeholder="{'hi':'a'}"
+				></textarea>
+				<br/>
+				<button onClick={handleJsonInput}>Handle Input</button>
+				<br/>
+
+				{/* <p>
+					The underlying circuit is from the <a href="https://github.com/iden3/snarkjs">snarkjs readme</a>
+				</p>
+				<pre>Witness Inputs</pre>
+
 				<Field label="Input a:">
 					<Input type="text" required={true} value={a} onChange={changeA} placeholder="e.g. 3" />
 				</Field>
 				<Field label="Input b:">
-					<Input type="text" required={true} value={b} onChange={changeB} placeholder="e.g. 11" />
-				</Field>
-				<Button.Outline onClick={runProofs}>Generate Proof</Button.Outline>
-				Proof: <Text width={1 / 2}>{proof}</Text>
-				Signals: <Text>{signals}</Text>
+					<input type="text" required={true} value={b} onChange={changeB} placeholder="e.g. 11" />
+				</Field> */}
+
+				<button onClick={runProofs}>Generate Proof</button>
+				<br/>
+				Proof: <p>{proof}</p>
+				Signals: <p>{signals}</p>
 				Result:
-				{proof.length > 0 && <Text>{isValid ? "Valid proof" : "Invalid proof"}</Text>}
+				{proof.length > 0 && <p>{isValid ? "Valid proof" : "Invalid proof"}</p>}
 			</header>
 		</div>
 	);
